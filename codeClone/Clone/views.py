109549f40cd73b0217ef96df35f1b230d16da702
divyansh-2707 <divyanshsingh11aa@gmail.com>
2024-12-11 00:21:48 +0530

    commit

--- codeClone/Clone/views.py
+++ codeClone/Clone/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from Hello.models import Contact
+from datetime import datetime
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+  return render(request,"home.html")
+
+def about(request):
+  return render(request,"about.html")
+
+def contactus(request):
+  return render(request,"contactus.html")
+
+def app(request):
+  return render(request,"app.html")
